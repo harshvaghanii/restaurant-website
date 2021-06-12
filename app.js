971cd7d233772877ec7656cba190175903611a0f
Harsh Vaghani <harshvaghani98@gmail.com>
2021-06-12 22:42:51 +0530

    Animating the Hamburger Menu

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
+var menuOpen = false;
 let hamMenu = () => {
     let x = document.getElementById("nav-link-ul");
 
     if (x.style.clipPath === "circle(900px at 95% -20%)") x.style.clipPath = "circle(100px at 95% -20%)";
 
     else x.style.clipPath = "circle(900px at 95% -20%)";
+
+
+    let l1 = document.getElementById("line1");
+    let l2 = document.getElementById("line2");
+    let l3 = document.getElementById("line3");
+
+    if (!menuOpen) {
+        menuOpen = true;
+        l1.style.transform = "rotate(-45deg) translate(-12px)";
+        l2.style.transform = "translate(-50px)";
+        l2.style.opacity = "0";
+        l3.style.transform = "rotate(45deg) translate(-12px)";
+    } else {
+        menuOpen = false;
+        l1.style.transform = "rotate(0deg) translate(0px)";
+        l3.style.transform = "rotate(0deg) translate(0px)";
+        l2.style.transform = "translate(0px)";
+        l2.style.opacity = "1";
+    }
+
+
 }
 
 
@@ -12,4 +34,14 @@ let hideMenu = () => {
         let x = document.getElementById("nav-link-ul");
         x.style.clipPath = "circle(100px at 95% -20%)";
     }
+
+    let l1 = document.getElementById("line1");
+    let l2 = document.getElementById("line2");
+    let l3 = document.getElementById("line3");
+
+    menuOpen = false;
+    l1.style.transform = "rotate(0deg) translate(0px)";
+    l3.style.transform = "rotate(0deg) translate(0px)";
+    l2.style.transform = "translate(0px)";
+    l2.style.opacity = "1";
 }
